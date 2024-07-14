fe7cea7dc6a0fc2f27998312bc7796045e8f8ef7
Timmy <yangtmeco@outlook.com>
2024-07-14 18:56:44 +0800

    change lazygit gitsigns to git.lua

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,79 @@
+return {
+    {
+        "kdheepak/lazygit.nvim",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Git: LazyGit" }
+        }
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "VeryLazy",
+        config = function()
+            require('gitsigns').setup {
+                on_attach = function(bufnr)
+                    local gitsigns = require('gitsigns')
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map('n', '<leader>hj', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ '<leader>hj', bang = true })
+                        else
+                            gitsigns.nav_hunk('next')
+                        end
+                    end, { desc = "Git: Next Hunk" })
+
+                    map('n', '<leader>hk', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ '<leader>hk', bang = true })
+                        else
+                            gitsigns.nav_hunk('prev')
+                        end
+                    end, { desc = "Git: Pre Hunk" })
+
+                    -- Actions
+                    map('n', '<leader>hs', gitsigns.stage_hunk, { desc = "Git: Stage Hunk" })
+                    map('n', '<leader>hr', gitsigns.reset_hunk, { desc = "Git: Reset Hunk" })
+                    map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+                        { desc = "Git: Stage Hunk" })
+                    map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+                        { desc = "Git: Reset Hunk" })
+                    map('n', '<leader>hS', gitsigns.stage_buffer, { desc = "Git: State Buffer" })
+                    map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = "Git: Undo Stage Hunk" })
+                    map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "Git: Reset Buffer" })
+                    map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "Git: Preview Hunk" })
+                    map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end,
+                        { desc = "Git: Blame Line" })
+                    map('n', '<leader>tb', gitsigns.toggle_current_line_blame,
+                        { desc = "Git: Toggle Current Line Blame" })
+                    map('n', '<leader>hd', gitsigns.diffthis, { desc = "Git: Diff This" })
+                    map('n', '<leader>hD', function() gitsigns.diffthis('~') end, { desc = "Git: Diff This ~" })
+                    map('n', '<leader>td', gitsigns.toggle_deleted, { desc = "Git: Toggle Deleted" })
+
+                    -- Text object
+                    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<cr>',
+                        { silent = true, desc = "Git: Select Hunk" })
+                end }
+        end
+
+    },
+
+}
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -50,62 +50,6 @@ return {
         }
     },
     {
-        "lewis6991/gitsigns.nvim",
-        event = "VeryLazy",
-        config = function()
-            require('gitsigns').setup {
-                on_attach = function(bufnr)
-                    local gitsigns = require('gitsigns')
-
-                    local function map(mode, l, r, opts)
-                        opts = opts or {}
-                        opts.buffer = bufnr
-                        vim.keymap.set(mode, l, r, opts)
-                    end
-
-                    -- Navigation
-                    map('n', '<leader>hj', function()
-                        if vim.wo.diff then
-                            vim.cmd.normal({ '<leader>hj', bang = true })
-                        else
-                            gitsigns.nav_hunk('next')
-                        end
-                    end, { desc = "[G][N][H] Next Hunk" })
-
-                    map('n', '<leader>hk', function()
-                        if vim.wo.diff then
-                            vim.cmd.normal({ '<leader>hk', bang = true })
-                        else
-                            gitsigns.nav_hunk('prev')
-                        end
-                    end, { desc = "[G][P][H] Pre Hunk" })
-
-                    -- Actions
-                    map('n', '<leader>hs', gitsigns.stage_hunk, { desc = "Gitsigns: Stage Hunk" })
-                    map('n', '<leader>hr', gitsigns.reset_hunk, { desc = "Gitsigns: Reset Hunk" })
-                    map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
-                        { desc = "Gitsigns: Stage Hunk" })
-                    map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
-                        { desc = "Gitsigns: Reset Hunk" })
-                    map('n', '<leader>hS', gitsigns.stage_buffer, { desc = "Gitsigns: State Buffer" })
-                    map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = "Gitsigns: Undo Stage Hunk" })
-                    map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "Gitsigns: Reset Buffer" })
-                    map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "Gitsigns: Preview Hunk" })
-                    map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end,
-                        { desc = "Gitsigns: Blame Line" })
-                    map('n', '<leader>tb', gitsigns.toggle_current_line_blame,
-                        { desc = "Gitsigns: Toggle Current Line Blame" })
-                    map('n', '<leader>hd', gitsigns.diffthis, { desc = "Gitsigns: Diff This" })
-                    map('n', '<leader>hD', function() gitsigns.diffthis('~') end, { desc = "Gitsigns: Diff This ~" })
-                    map('n', '<leader>td', gitsigns.toggle_deleted, { desc = "Gitsigns: Toggle Deleted" })
-
-                    -- Text object
-                    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<cr>', { silent = true, desc = "Gitsigns: Select Hunk" })
-                end }
-        end
-
-    },
-    {
         "goolord/alpha-nvim",
         config = function()
             require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
