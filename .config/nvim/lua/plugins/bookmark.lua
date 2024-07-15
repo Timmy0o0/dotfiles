0b5bf519e1c818e3c12e3e33c56206b5b986e33e
Timmy <yangtmeco@outlook.com>
2024-07-16 00:17:01 +0800

    change bookmark vim plugin to nvim

--- .config/nvim/lua/plugins/bookmark.lua
+++ .config/nvim/lua/plugins/bookmark.lua
@@ -1,28 +1,28 @@
 return {
-    'tom-anders/telescope-vim-bookmarks.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-        'MattesGroeger/vim-bookmarks',
-    },
-    config = function()
-        vim.cmd([[highlight BookmarkSign ctermbg=NONE ctermfg=160]])
-        vim.cmd([[highlight BookmarkLine ctermbg=194 ctermfg=NONE]])
-        vim.g.bookmark_sign = '♥'
-        vim.g.bookmark_highlight_lines = 1
-
-        require('telescope').load_extension('vim_bookmarks')
-
-        vim.keymap.set(
-            { 'n' },
-            '<leader>fm',
-            '<cmd>lua require("telescope").extensions.vim_bookmarks.all()<cr>'
-        )
-        vim.keymap.set(
-            { 'n' },
-            '<leader>fM',
-            '<cmd>lua require("telescope").extensions.vim_bookmarks.current_file()<cr>'
-        )
-    end,
+    -- 'tom-anders/telescope-vim-bookmarks.nvim',
+    -- event = 'VeryLazy',
+    -- dependencies = {
+    --     'MattesGroeger/vim-bookmarks',
+    -- },
+    -- config = function()
+    --     vim.cmd([[highlight BookmarkSign ctermbg=NONE ctermfg=160]])
+    --     vim.cmd([[highlight BookmarkLine ctermbg=194 ctermfg=NONE]])
+    --     vim.g.bookmark_sign = '♥'
+    --     vim.g.bookmark_highlight_lines = 1
+    --
+    --     require('telescope').load_extension('vim_bookmarks')
+    --
+    --     vim.keymap.set(
+    --         { 'n' },
+    --         '<leader>fm',
+    --         '<cmd>lua require("telescope").extensions.vim_bookmarks.all()<cr>'
+    --     )
+    --     vim.keymap.set(
+    --         { 'n' },
+    --         '<leader>fM',
+    --         '<cmd>lua require("telescope").extensions.vim_bookmarks.current_file()<cr>'
+    --     )
+    -- end,
 
     -- keymap
     -- mm | Add/remove bookmark at current line
@@ -36,4 +36,34 @@ return {
     -- [count]mjj | Move down bookmark at current line
     -- [count]mg | Move bookmark at current line to another line
     --
+    -- sign_priority = 8,
+    'tomasky/bookmarks.nvim',
+    event = 'VimEnter',
+    config = function()
+        require('telescope').load_extension('bookmarks')
+        vim.keymap.set({ 'n' }, '<leader>fm', '<cmd>Telescope bookmarks list<cr>')
+
+        require('bookmarks').setup({
+            sign_priority = 8,
+            save_file = vim.fn.expand('$HOME/.bookmarks'), -- bookmarks save file path
+            keywords = {
+                ['@t'] = ' ', -- mark annotation startswith @t ,signs this icon as `Todo`
+                ['@w'] = ' ', -- mark annotation startswith @w ,signs this icon as `Warn`
+                ['@f'] = ' ', -- mark annotation startswith @f ,signs this icon as `Fix`
+                ['@n'] = ' ', -- mark annotation startswith @n ,signs this icon as `Note`
+            },
+            ---@diagnostic disable-next-line: unused-local
+            on_attach = function(bufnr)
+                local bm = require('bookmarks')
+                local map = vim.keymap.set
+                map('n', 'mm', bm.bookmark_toggle) -- add or remove bookmark at current line
+                map('n', 'mi', bm.bookmark_ann) -- add or edit mark annotation at current line
+                map('n', 'mc', bm.bookmark_clean) -- clean all marks in local buffer
+                map('n', 'mn', bm.bookmark_next) -- jump to next mark in local buffer
+                map('n', 'mp', bm.bookmark_prev) -- jump to previous mark in local buffer
+                map('n', 'ml', bm.bookmark_list) -- show marked file list in quickfix window
+                map('n', 'mx', bm.bookmark_clear_all) -- removes all bookmarks
+            end,
+        })
+    end,
 }
