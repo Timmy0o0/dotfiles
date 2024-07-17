4d40d0441c2012f7653dcee371f5435f49191995
Timmy <yangtmeco@outlook.com>
2024-07-17 22:11:18 +0800

    add Telescope keymap search for git status

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -57,7 +57,7 @@ return {
         vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = 'Telescope: Find opened buffers' })
         vim.keymap.set('n', '<leader>?', builtin.help_tags, { desc = 'Telescope: Find help files' })
         vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope: Find lsp diagnostics' })
-        -- vim.keymap.set('n', '<leader>fh', builtin.git_status, { desc = 'Telescope: Find recently opened files' })
+        vim.keymap.set('n', '<leader>fH', builtin.git_status, { desc = 'Telescope: Find git status files' })
         vim.keymap.set('n', '<leader>fh', "<cmd>Telescope git_signs<cr>", { desc = 'Telescope: Find git_signs' })
         -- stylua: ignore end
         vim.keymap.set('n', '<leader>/', function()
