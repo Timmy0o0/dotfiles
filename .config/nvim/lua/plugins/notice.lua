e2d497d72c43da9c2a7a5e6c9228eafc8436af97
Timmy <yangtmeco@outlook.com>
2024-07-17 11:40:09 +0800

    config notice

--- .config/nvim/lua/plugins/notice.lua
+++ .config/nvim/lua/plugins/notice.lua
@@ -10,6 +10,25 @@ return {
             view_history = 'messages',
             view_search = 'virtualtext',
         },
+        lsp = {
+            progress = {
+                enabled = false,
+            },
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+                ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                ['vim.lsp.util.stylize_markdown'] = true,
+                ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+            },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+            bottom_search = true,
+            command_palette = false,
+            long_message_to_split = true,
+            inc_rename = false,
+            lsp_doc_border = false,
+        },
     },
     dependencies = {
         'MunifTanjim/nui.nvim',
