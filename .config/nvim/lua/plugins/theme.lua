6c56f4942cf959cd1756ba24db4050f7ec3b840a
Timmy <yangtmeco@outlook.com>
2024-07-17 20:16:03 +0800

    add gruvbox-material

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -31,22 +31,17 @@ return {
         end,
     },
     {
-        'Mofiqul/vscode.nvim',
+        'sainnhe/gruvbox-material',
         lazy = false,
         priority = 1000,
         config = function()
-            local c = require('vscode.colors').get_colors()
-            -- vim.o.background = 'light'
-            require('vscode').setup({
-                transparent = false,
-                italic_comments = true,
-                disable_nvimtree_bg = true,
-                underline_links = true,
-                group_overrides = {
-                    Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-                },
-            })
-            -- vim.cmd.colorscheme('vscode')
+            vim.opt.background = 'light'
+            vim.g.gruvbox_material_background = 'soft' -- medium | hard | soft
+            vim.g.gruvbox_material_foreground = 'material'
+            vim.g.gruvbox_material_enable_italic = true
+            vim.g.gruvbox_material_enable_bold = 1
+            vim.g.gruvbox_material_transparent_background = 0
+            -- vim.cmd.colorscheme('gruvbox-material')
         end,
     },
     {
