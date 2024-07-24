7797cec08279ff8de381e6b470b638ce90acb16a
Timmy <yangtmeco@outlook.com>
2024-07-25 07:53:31 +0800

    feat: add lualine bubble theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -59,16 +59,43 @@ return {
         },
         config = function()
             require('lualine').setup({
+                -- origin
+                -- options = {
+                --     theme = 'auto',
+                -- },
+                -- sections = {
+                --     lualine_a = { 'mode' },
+                --     lualine_b = { 'branch', 'diff', 'diagnostics' },
+                --     lualine_c = { { 'filename', path = 4 } },
+                --     lualine_x = { 'filetype' },
+                --     -- lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                --     lualine_y = { 'progress' },
+                --     lualine_z = { 'location' },
+                -- },
+
+                -- bubbles
                 options = {
-                    theme = 'auto',
+                    component_separators = '',
+                    section_separators = { left = '', right = '' },
                 },
                 sections = {
-                    lualine_a = { 'mode' },
+                    lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
                     lualine_b = { 'branch', 'diff', 'diagnostics' },
-                    lualine_c = { { 'filename', path = 4 } },
-                    lualine_x = { 'filetype' },
-                    -- lualine_x = { 'encoding', 'fileformat', 'filetype' },
-                    lualine_y = { 'progress' },
+                    lualine_c = {
+                        { 'filename', path = 4 },
+                    },
+                    lualine_x = {},
+                    lualine_y = { 'filetype', 'progress' },
+                    lualine_z = {
+                        { 'location', separator = { right = '' }, left_padding = 2 },
+                    },
+                },
+                inactive_sections = {
+                    lualine_a = { 'filename' },
+                    lualine_b = {},
+                    lualine_c = {},
+                    lualine_x = {},
+                    lualine_y = {},
                     lualine_z = { 'location' },
                 },
             })
