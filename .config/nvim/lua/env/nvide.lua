b325cea663e01bf32079849c1e9c373099116815
Timmy <yangtmeco@outlook.com>
2024-11-10 19:30:17 +0800

    feat: neovide config for linux

--- .config/nvim/lua/env/nvide.lua
+++ .config/nvim/lua/env/nvide.lua
@@ -1,15 +1,15 @@
 local is_mac = vim.fn.has('macunix') == 1
+local is_win = vim.fn.has('win32') == 1
 
 if vim.g.neovide then
     if is_mac then
         -- option.guifont = "SauceCodePro Nerd Font:h18"
         -- option.guifont = "FiraCode Nerd Font:h18"
-        -- option.guifont = "JetBrainsMono Nerd Font,Noto Color Emoji:h18"
         vim.opt.guifont = 'JetBrainsMono Nerd Font:h18'
-    else
-        -- option.guifont = "SauceCodePro Nerd Font:h13"
-        -- option.guifont = "FiraCode Nerd Font:h13"
+    elseif is_win then
         vim.opt.guifont = 'JetBrainsMono Nerd Font,Segoe UI Emoji:h14'
+    else
+        vim.opt.guifont = 'JetBrainsMono Nerd Font,Noto Color Emoji:h14'
     end
 
     vim.g.neovide_transparency = 0.95
