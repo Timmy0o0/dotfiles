339846924c2789fd15748434d3aa7c6ea548c75c
Timmy <yangtmeco@outlook.com>
2024-11-26 21:26:46 +0800

    feat: pnpm env config for fish

--- .config/fish/conf.d/00-env.fish
+++ .config/fish/conf.d/00-env.fish
@@ -2,3 +2,10 @@
 # ENV VARS
 # -----------------------------------------------------
 set -x EDITOR nvim
+
+# pnpm
+set -gx PNPM_HOME "/home/timmy/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
