99542131d0d525197b8cced4417044e7c9693df2
Timmy <yangtmeco@outlook.com>
2024-12-26 10:44:56 +0800

    feat:feat: add overseer to lualine

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -1,9 +1,15 @@
 return {
   "nvim-lualine/lualine.nvim",
-  opts = {
-    options = {
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-    },
-  },
+  opts = function(_, opts)
+    opts.options = opts.options or {}
+    -- opts.options.component_separators = { left = "", right = "" }
+    -- opts.options.section_separators = { left = "", right = "" }
+    -- opts.options.component_separators = { left = "┃", right = "┃" }
+    opts.options.component_separators = { left = "│", right = "│" }
+    opts.options.section_separators = { left = "", right = "" }
+
+    opts.sections = opts.sections or {}
+    opts.sections.lualine_x = opts.sections.lualine_x or {}
+    table.insert(opts.sections.lualine_x, "overseer")
+  end,
 }
