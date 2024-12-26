05e7221aacfca03eb7e34b7bf6b6f72a24dbbaff
Timmy <yangtmeco@outlook.com>
2024-12-26 11:29:17 +0800

    feat: add fzf-lua key

--- .config/nvim/lua/plugins/utils/fzf-lua.lua
+++ .config/nvim/lua/plugins/utils/fzf-lua.lua
@@ -2,7 +2,6 @@ return {
   "ibhagwan/fzf-lua",
   keys = {
     { "<leader>,", false },
-    { "<leader>/", false },
     { "<leader>:", false },
     { "<leader>sb", false },
     { "<leader>sg", false },
@@ -11,6 +10,7 @@ return {
     { "<leader>sR", false },
     { "<leader>sM", false },
     { "<leader><space>", "<cmd>FzfLua buffers<cr>", desc = "Find Buffers" },
+    { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "Grep Current Buffer" },
     { "<leader>fw", "<cmd>FzfLua live_grep<cr>", desc = "Grep (Root Dir)" },
   },
 }
