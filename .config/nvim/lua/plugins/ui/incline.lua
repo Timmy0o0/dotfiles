e7a62d6ff457d41a7506c20ebfd5f588f476355f
Timmy <yangtmeco@outlook.com>
2024-12-31 21:28:53 +0800

    feat: change incline config

--- .config/nvim/lua/plugins/ui/incline.lua
+++ .config/nvim/lua/plugins/ui/incline.lua
@@ -5,24 +5,42 @@ return {
     local devicons = require("nvim-web-devicons")
     local helpers = require("incline.helpers")
 
+    -- helper function to format path: show full start path and last 2 parent directories
+    local function format_path(buf_name)
+      local path = vim.fn.fnamemodify(buf_name, ":~:.")
+      local parts = vim.split(path, "/")
+
+      -- if path has more than 4 parts, show first part + ... + last 3 parts
+      if #parts > 4 then
+        return parts[1] .. "/.../" .. table.concat({ parts[#parts - 2], parts[#parts - 1], parts[#parts] }, "/")
+      end
+      return path
+    end
+
     return {
       window = {
         padding = 0,
         margin = { horizontal = 0 },
       },
       render = function(props)
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        if filename == "" then
-          filename = "[No Name]"
+        -- get relative path instead of just filename
+        local filepath = format_path(vim.api.nvim_buf_get_name(props.buf))
+        if filepath == "" then
+          filepath = "[No Name]"
         end
-        local ft_icon, ft_color = devicons.get_icon_color(filename)
+
+        -- get icon for the file
+        local ft_icon, ft_color = devicons.get_icon_color(filepath)
         local modified = vim.bo[props.buf].modified
+
+        -- add modified indicator
+        local modified_indicator = modified and " [+]" or ""
+
         return {
           ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
           " ",
-          { filename, gui = modified and "bold,italic" or "bold" },
+          { filepath .. modified_indicator, gui = modified and "bold,italic" or "bold" },
           " ",
-          -- guibg = "#44406e",
         }
       end,
     }
