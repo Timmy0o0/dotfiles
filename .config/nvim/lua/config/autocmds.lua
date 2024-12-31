55478e8dc11c898f748154f01434030a740846b7
Timmy <yangtmeco@outlook.com>
2024-12-31 20:08:52 +0800

    feat: remove auto format for typescript

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,19 +7,19 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.ts,*.tsx,*.jsx,*.js",
-  callback = function(args)
-    -- First organize imports with proper context
-    vim.lsp.buf.code_action({
-      context = {
-        only = { "source.organizeImports" },
-        diagnostics = {}, -- Add empty diagnostics array
-      },
-      apply = true,
-    })
-    require("conform").format({ bufnr = args.buf })
-    -- Then sort tailwind classes
-    -- vim.cmd("TailwindSortSync")
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.ts,*.tsx,*.jsx,*.js",
+--   callback = function(args)
+--     -- First organize imports with proper context
+--     vim.lsp.buf.code_action({
+--       context = {
+--         only = { "source.organizeImports" },
+--         diagnostics = {}, -- Add empty diagnostics array
+--       },
+--       apply = true,
+--     })
+--     require("conform").format({ bufnr = args.buf })
+--     -- Then sort tailwind classes
+--     -- vim.cmd("TailwindSortSync")
+--   end,
+-- })
