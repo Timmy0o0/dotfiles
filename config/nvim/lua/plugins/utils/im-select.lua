d2cfbd54750708484066c88396b07d9cce1a73d3
timmy <yangtmeco@outlook.com>
2025-04-16 18:33:26 +0800

    feat: change im-select for multi os

--- config/nvim/lua/plugins/utils/im-select.lua
+++ config/nvim/lua/plugins/utils/im-select.lua
@@ -1,39 +1,53 @@
 return {
   "keaising/im-select.nvim",
   event = "VeryLazy",
-  opts = {
+  opts = function()
+    -- Initialize variables for input method select and command
+    local default_im_select
+    local default_command
 
-    -- IM will be set to `default_im_select` in `normal` mode
-    -- For Windows/WSL, default: "1033", aka: English US Keyboard
-    -- For macOS, default: "com.apple.keylayout.ABC", aka: US
-    -- For Linux, default:
-    --               "keyboard-us" for Fcitx5
-    --               "1" for Fcitx
-    --               "xkb:us::eng" for ibus
-    -- You can use `im-select` or `fcitx5-remote -n` to get the IM's name
-    default_im_select = "keyboard-us",
+    -- Get current system name
+    local system = vim.uv.os_uname().sysname
 
-    -- Can be binary's name, binary's full path, or a table, e.g. 'im-select',
-    -- '/usr/local/bin/im-select' for binary without extra arguments,
-    -- or { "AIMSwitcher.exe", "--imm" } for binary need extra arguments to work.
-    -- For Windows/WSL, default: "im-select.exe"
-    -- For macOS, default: "macism"
-    -- For Linux, default: "fcitx5-remote" or "fcitx-remote" or "ibus"
-    default_command = "fcitx5-remote",
+    -- Auto detect system and set appropriate values
+    if system == "Windows" or system == "Windows_NT" or string.find(vim.loop.os_uname().release:lower(), "microsoft") then
+      -- Windows or WSL
+      default_im_select = "1033" -- English US Keyboard
+      default_command = "im-select.exe"
+    elseif system == "Darwin" then
+      -- For macOS, default: "com.apple.keylayout.ABC", aka: US
+      default_im_select = "com.apple.keylayout.ABC"
+      default_command = "macism"
+    else
+      -- Linux
+      -- Check available input method frameworks
+      local has_fcitx5 = vim.fn.executable("fcitx5-remote") == 1
+      local has_ibus = vim.fn.executable("ibus") == 1
 
-    -- Restore the default input method state when the following events are triggered
-    set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
+      -- Set appropriate values based on available input method
+      if has_fcitx5 then
+        default_im_select = "keyboard-us"
+        default_command = "fcitx5-remote"
+      elseif has_ibus then
+        default_im_select = "xkb:us::eng"
+        default_command = "ibus"
+      else
+        -- Default values if no input method is detected
+        default_im_select = "keyboard-us"
+        default_command = "fcitx5-remote"
+      end
+    end
 
-    -- Restore the previous used input method state when the following events
-    -- are triggered, if you don't want to restore previous used im in Insert mode,
-    -- e.g. deprecated `disable_auto_restore = 1`, just let it empty
-    -- as `set_previous_events = {}`
-    set_previous_events = {},
-
-    -- Show notification about how to install executable binary when binary missed
-    keep_quiet_on_no_binary = false,
-
-    -- Async run `default_command` to switch IM or not
-    async_switch_im = true,
-  },
+    return {
+      default_im_select = default_im_select,
+      default_command = default_command,
+      -- Restore the default input method state when the following events are triggered
+      set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
+      set_previous_events = {},
+      -- Show notification about how to install executable binary when binary missed
+      keep_quiet_on_no_binary = false,
+      -- Async run `default_command` to switch IM or not
+      async_switch_im = true,
+    }
+  end,
 }
