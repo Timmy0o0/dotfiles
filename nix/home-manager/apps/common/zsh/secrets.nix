387f2ee98401cb0b697b22df18525df1e3b85b36
timmy <yangtmeco@outlook.com>
2025-04-16 21:06:40 +0800

    feat: upload api key

--- nix/home-manager/apps/common/zsh/secrets.nix
+++ nix/home-manager/apps/common/zsh/secrets.nix
@@ -2,7 +2,7 @@
 
 {
   programs.zsh.sessionVariables = {
-    GEMINI_API_KEY = "";
-    DEEPSEEK_API_KEY = "";
+    GEMINI_API_KEY = "AIzaSyAjRJmxPaYmXP_svZDOvSFGKi-jyIdcM3c";
+    DEEPSEEK_API_KEY = "sk-fe4f4c790f1d4b618259301d2fdcbead";
   };
 }
