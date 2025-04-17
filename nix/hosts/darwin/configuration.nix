2d1b914f81878eeb5214c94f145fc976b2a18116
timmy <yangtmeco@outlook.com>
2025-04-17 18:42:02 +0800

    feat：gui app link

--- nix/hosts/darwin/configuration.nix
+++ nix/hosts/darwin/configuration.nix
@@ -3,10 +3,8 @@
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  # nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = [
-    # pkgs.alacritty
-  ];
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = [ pkgs.mkalias pkgs.kitty pkgs.obsidian ];
 
   users.users.timmy = {
     name = "timmy";
@@ -16,59 +14,45 @@
   # GUI apps
   homebrew = {
     enable = true;
+    #   brews = [
+    #     # search tool
+    #     "mas"
+    #   ];
     casks = [ "cursor" ];
+    #   taps = [
+    #   ];
+    #   masApps = {
+    #     Yoink = 457622435;
+    #   };
+    onActivation = {
+      cleanup = "zap";
+      autoUpdate = true;
+      upgrade = true;
+    };
   };
 
-  # homebrew = {
-  #   enable = true;
-  #   brews = [
-  #     "mas"
-  #   ];
-  #   casks = [
-  #     "hammerspoon"
-  #     "amethyst"
-  #     "alfred"
-  #     "logseq"
-  #     "notion"
-  #     "firefox"
-  #     "discord"
-  #     "iina"
-  #     "the-unarchiver"
-  #   ];
-  #   taps = [
-  #   ];
-  #   masApps = {
-  #     Yoink = 457622435;
-  #   };
-  # };
-
-  # system.activationScripts.applications.text = let
-  #   env = pkgs.buildEnv {
-  #     name = "system-applications";
-  #     paths = config.environment.systemPackages;
-  #     pathsToLink = "/Applications";
-  #   };
-  # in pkgs.lib.mkForce ''
-  #   # Set up applications.
-  #   echo "setting up /Applications..." >&2
-  #   rm -rf /Applications/Nix\ Apps
-  #   mkdir -p /Applications/Nix\ Apps
-  #   find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-  #   while read -r src; do
-  #     app_name=$(basename "$src")
-  #     echo "copying $src" >&2
-  #     ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-  #   done
-  # '';
+  system.activationScripts.applications.text = let
+    env = pkgs.buildEnv {
+      name = "system-applications";
+      paths = config.environment.systemPackages;
+      pathsToLink = "/Applications";
+    };
+  in pkgs.lib.mkForce ''
+    # Set up applications.
+    echo "setting up /Applications..." >&2
+    rm -rf /Applications/Nix\ Apps
+    mkdir -p /Applications/Nix\ Apps
+    find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+    while read -r src; do
+      app_name=$(basename "$src")
+      echo "copying $src" >&2
+      ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+    done
+  '';
 
   # Auto upgrade nix package and the daemon service.
   # services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
-
-  # services.postgresql = {
-  #   enable = true;
-  #   package = pkgs.postgresql_16;
-  # };
 
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
