47a6b5f544ba7a21732dc7f00dd39d678078585c
timmy <yangtmeco@outlook.com>
2025-04-22 12:53:59 +0800

    chore(nvim): Disable win32yank configuration

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -3,19 +3,19 @@
 -- Add any additional options here
 
 -- wsl win32yank
-if vim.fn.executable("win32yank.exe") == 1 then
-  vim.g.clipboard = {
-    name = "win32yank-wsl",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-  }
-else
-  print("win32yank.exe not found in PATH or is not executable from WSL.")
-end
+-- if vim.fn.executable("win32yank.exe") == 1 then
+--   vim.g.clipboard = {
+--     name = "win32yank-wsl",
+--     copy = {
+--       ["+"] = "win32yank.exe -i --crlf",
+--       ["*"] = "win32yank.exe -i --crlf",
+--     },
+--     paste = {
+--       ["+"] = "win32yank.exe -o --lf",
+--       ["*"] = "win32yank.exe -o --lf",
+--     },
+--     cache_enabled = 0,
+--   }
+-- else
+--   print("win32yank.exe not found in PATH or is not executable from WSL.")
+-- end
