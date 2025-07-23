f858db3a8bd1d8cce49a863af660a1fdc0598d76
timmy <yangtmeco@outlook.com>
2025-07-23 09:02:01 +0800

    feat: add yazi and starship config file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,16 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-starship init fish | source
-
-function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
-end
-
 alias c="clear"
 alias lg="lazygit"
