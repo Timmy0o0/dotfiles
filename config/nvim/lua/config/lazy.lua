313eb2954d35dfca34debcae33ef29fcf1810f51
timmy <yangtmeco@outlook.com>
2026-04-18 01:41:48 +0800

    feat: add im-select

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "plugins" },
     { import = "plugins.coding" },
     { import = "plugins.ui" },
+    { import = "plugins.utils" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
