9d5d494838cb698ff8fd456ffcce6f34a1954b53
timmy <yangtmeco@outlook.com>
2026-04-18 23:29:03 +0800

    feat: eza

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,3 +6,7 @@ end
 alias c="clear"
 alias lg="lazygit"
 alias oc="opencode --port"
+alias ls="eza --icons"
+alias ll="eza -l --icons --git"
+alias la="eza -la --icons --git"
+alias tree="eza --tree --icons"
