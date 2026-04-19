0f30635cdc0088eadbbc664be84abc7a2b5978eb
timmy <yangtmeco@outlook.com>
2026-04-19 11:57:18 +0800

    feat: add dial remove alternate-toggle and wildfire

--- config/nvim/lua/plugins/coding/alternate-toggler.lua
+++ config/nvim/lua/plugins/coding/alternate-toggler.lua
@@ -1,3 +1,6 @@
+-- stylua: ignore
+if true then return {} end
+
 return {
   "rmagatti/alternate-toggler",
   event = { "BufReadPost" },
--- config/nvim/lua/plugins/coding/wildfire.lua
+++ config/nvim/lua/plugins/coding/wildfire.lua
@@ -1,3 +1,6 @@
+-- stylua: ignore
+if true then return {} end
+
 return {
   "sustech-data/wildfire.nvim",
   event = "VeryLazy",
